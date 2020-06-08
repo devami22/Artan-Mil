4b983768711804f10300ac4f064c641adc52cd60
devami22 <62856013+devami22@users.noreply.github.com>
2020-06-08 11:55:50 -0700

    style changes

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //dice ONE
-var randomNumber2 = Math.floor(Math.random() * 6) + 1; //dice TWO
+
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1; //dice ONE
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1; //dice TWO
 
 
-var randomDice1 = "dice" + randomNumber1 + ".png";
-var randomDice2 = "dice" + randomNumber2 + ".png";
+    var randomDice1 = "dice " + randomNumber1 + ".png";
+    var randomDice2 = "dice " + randomNumber2 + ".png";
 
-var imageSource1 = "images/" + randomDice1;
-var imageSource2 = "images/" + randomDice2;
+    var imageSource1 = "images/" + randomDice1;
+    var imageSource2 = "images/" + randomDice2;
 
 
-var diceOne = document.querySelectorAll("img")[0];
+    var diceOne = document.querySelectorAll("img")[0];
 
-diceOne.setAttribute("src", imageSource1);
+    diceOne.setAttribute("src", imageSource1);
 
-var diceTwo = document.querySelectorAll('img')[1];
+    var diceTwo = document.querySelectorAll('img')[1];
 
-diceTwo.setAttribute('src', imageSource2);
+    diceTwo.setAttribute('src', imageSource2);
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector('h1').innerHTML = "🚩Player 1 Wins!"
-}
-else if (randomNumber2 > randomNumber1) {
-    document.querySelector('h1').innerHTML = "Player 2 Wins! 🚩"
-}
-else {
-    document.querySelector('h1').innerHTML = "Draw!"
-}
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector('h1').innerHTML = "Player 1 Wins!"
+    }
+    else if (randomNumber2 > randomNumber1) {
+        document.querySelector('h1').innerHTML = "Player 2 Wins!"
+    }
+    else {
+        document.querySelector('h1').innerHTML = "Draw!"
+    };
